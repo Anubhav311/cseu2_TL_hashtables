b3ddfecdcd06509f40de664be405d6f9065b72a5
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-21 17:51:42 +0530

    created firebaseConfig file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyByte5nh7cajduAOegw-DmqRupfHQWnyfk",
+    authDomain: "test-frbs-6650b.firebaseapp.com",
+    databaseURL: "https://test-frbs-6650b.firebaseio.com",
+    projectId: "test-frbs-6650b",
+    storageBucket: "test-frbs-6650b.appspot.com",
+    messagingSenderId: "668938698072",
+    appId: "1:668938698072:web:2aefef4677ed827592f92d"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
