c257a022002f06fd8c67b7f180d56843e314c9ca
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-23 18:30:29 +0530

    created DynamicArray class

---
+++ array.py
@@ -0,0 +1,29 @@
+# THINGS WE NEED FOR AN ADVANCE ARRAY
+# size of it and protection from going past that
+# size of objects
+# to append need space
+# track allocated and used
+# if we run out of space, we need to make a new one with more space
+# then copy each item over
+
+class DynamicArray:
+    def __init__(self, capacity=8):
+        pass
+
+    def append(self, value):
+        pass
+
+    def insert(self, value, index):
+        pass
+
+    def remove(self, index):
+        pass
+
+    def print(self):
+        pass
+
+    def resize_array(self):
+        pass
+
+    def add_to_front(self, value):
+        pass
